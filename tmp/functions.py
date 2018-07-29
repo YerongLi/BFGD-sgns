b6b16b0588327df586ca1ffb8813a7c0e3e56a81
Yerong <yerong.li@outlook.com>
2018-07-29 15:52:16 -0500

    backup

---
+++ tmp/functions.py
@@ -0,0 +1,299 @@
+import numpy as np
+import os
+
+from scipy.sparse.linalg import svds
+from scipy.spatial.distance import cosine
+from scipy.stats import spearmanr
+
+from util.word2vec_as_MF import Word2vecMF
+from numpy.linalg import norm, svd
+           
+            
+def load_sentences(mode='debug'):
+    """
+    Load training corpus sentences/
+    """
+    
+    if (mode == 'imdb'):
+        sentences = pickle.load(open('data/sentences_all.txt', 'rb'))
+    elif (mode == 'debug'):
+        sentences = pickle.load(open('data/sentences1k.txt', 'rb'))
+    elif (mode == 'enwik9'):
+        sentences = pickle.load(open('data/enwik9_sentences.txt', 'rb'))
+    return sentences
+
+def plot_MF(MFs, x=None, xlabel='Iterations', ylabel='MF'):
+    """
+    Plot given MFs.
+    """
+    
+    fig, ax = plt.subplots(figsize=(15, 5))
+    if not x:
+        ax.plot(MFs)
+    else:
+        ax.plot(x, MFs)
+    ax.set_xlabel(xlabel, fontsize=14)
+    ax.set_ylabel(ylabel, fontsize=14)
+    ax.grid(True)
+    
+def norm_p2(A):
+    _, s, _ = svd(A)
+    return np.sqrt(sum(np.square(s)))
+
+################################## Projector splitting experiment ##################################
+def opt_experiment(model,
+                   mode='PS',
+                   min_count=200,
+                   d = 100,
+                   eta = 5e-6,
+                   reg = 0.,
+                   batch_size = 1000,
+                   MAX_ITER = 100,
+                   from_iter = 0,
+                   start_from = 'RAND',
+                   display = False,
+                   autostop = False,
+                   init = (False, None, None),
+                   itv_print=100,
+                   itv_save=5000,
+                   tol=80,           
+                  ):
+    """
+    Aggregator for projector splitting experiment.
+    """ 
+    
+    # Start projector splitting from svd of SPPMI or random initialization
+    
+    def folder_path(num_iter):
+        #return 'enwik-'+str(min_count)+'/'+mode+str(num_iter)+'iter_from'+start_from+'_dim'+str(d)+'_step'+str(eta)+'_factors'
+        return 'enwik-'+str(min_count)+'/'+mode+'iter_from'+start_from+'_dim'+str(d)+'_step'+str(eta)+'_'+str(reg)
+
+    
+    from_folder = folder_path(from_iter+MAX_ITER)
+    if (from_iter > 0):
+        os.rename(folder_path(from_iter), from_folder)  
+        C, W = model.load_CW(from_folder, from_iter)
+        init_ = (True, C, W)
+    else:
+        init_ = init
+    print(from_folder)
+    
+    if (mode == 'PS'):
+        model.projector_splitting(eta=eta, d=d, MAX_ITER=MAX_ITER, from_iter=from_iter, display=display,
+                                  init=init_, save=(True, from_folder), itv_print=itv_print, itv_save=itv_save, 
+                                  autostop= autostop, tol=tol)
+        
+    if (mode == 'SPS'):
+        model.stochastic_ps(eta=eta, batch_size=batch_size, d=d, MAX_ITER=MAX_ITER, from_iter=from_iter, display=display,
+                            init=init_, save=(True, from_folder))
+        
+    if (mode == 'AM'):
+        model.alt_min(eta=eta, d=d, MAX_ITER=MAX_ITER, from_iter=from_iter,
+                      init=init_, save=(True, from_folder))
+        
+    if (mode == 'BFGD'):
+
+        model.bfgd(eta=eta, d=d, reg=reg, MAX_ITER=MAX_ITER, from_iter=from_iter, display=display,
+                      init=init_, save=(True, from_folder), itv_print=itv_print, itv_save=itv_save,
+                      autostop=autostop, tol=tol)
+    
+
+
+################################## SPPMI decomposition initialization ##################################
+
+def SPPMI_init(model, dimension):
+    
+    SPPMI = np.maximum(np.log(model.D) - np.log(model.B), 0)
+    # SPPMI = np.log(model.D) - np.log(model.B)
+    
+    print(np.count_nonzero(SPPMI)/SPPMI.shape[0]**2)
+    print(norm(SPPMI, 'fro'))
+    print(SPPMI[0], 'SPPMI')
+    print(np.log(model.D)[0], 'logD')
+    print(np.log(model.B)[0], 'logB')
+    L=norm((model.B+model.D)/4, 'fro')
+    u, s, vt = svds(SPPMI, k=dimension)
+    C0 = u.dot(np.sqrt(np.diag(s))).T
+    W0 = np.sqrt(np.diag(s)).dot(vt)
+   
+    norm1=norm(np.concatenate((C0.T, W0.T), axis=0), ord=2)
+    norm2=norm(model.grad_MF(C0, W0), ord=2)
+    step_size = 1/(20*L*(norm1**2)+3*norm2)    
+    print('Initial loss', model.MF(C0, W0), 'theoretical step size', step_size)
+    
+    return C0, W0, step_size
+
+################################## Bi-Factorized Gradient Descent initialization ##################################
+def BFGD_init(model, dimension, reg=0):
+    L=norm((model.B+model.D)/4, 'fro')
+    
+    '''
+    X0=C0.T @ W0,  Vc x Vw
+    ''' 
+    X0 = 1/L*model.grad_MF(
+    np.zeros([dimension,model.B.shape[0]]), np.zeros([dimension,model.B.shape[1]]))
+    
+    print(X0.shape)
+    
+    u, s, vt = svds(X0, k=dimension)
+    
+    '''
+    C0, context matrix, d x Vc
+    W0,    word matrix, d x Vw
+    '''
+    C0 = u.dot(np.sqrt(np.diag(s))).T
+    W0 = np.sqrt(np.diag(s)).dot(vt)
+    
+    step_size=None
+    
+    if reg==0:
+        norm1=norm(np.concatenate((C0.T, W0.T), axis=0), ord=2)
+        norm2=norm(model.grad_MF(C0, W0), ord=2)
+        step_size = 1/(20*L*(norm1**2)+3*norm2)
+        
+    print('Initial loss', model.MF(C0, W0), 'theoretical step size', step_size)
+    
+    
+    return C0, W0, step_size
+
+
+
+def plot_dynamics(vecs, vec2, n=5, MAX_ITER=100):
+    """
+    Plot how the distances between pairs change with each n
+    iterations of the optimization method.
+    """
+    
+    for i in xrange(MAX_ITER):
+        if (i%n==0):
+            plt.clf()
+            plt.xlim([-0.01, 1.2])
+            plt.plot(vecs[i], vec2, 'ro', color='blue')
+            plt.grid()
+            display.clear_output(wait=True)
+            display.display(plt.gcf())
+    plt.clf()
+    
+    fig, ax = plt.subplots(1, 2, figsize=(13, 4))
+    for i in xrange(2):
+        ax[i].set_xlim([-0.01, 1.2])
+        ax[i].plot(vecs[-i], vec2, 'ro', color='blue')
+        ax[i].set_title(str(i*MAX_ITER)+' iterations')
+        ax[i].set_xlabel('Cosine distance', fontsize=14)
+        ax[i].set_ylabel('Assesor grade', fontsize=14)
+        ax[i].grid()
+    
+    
+def dist_change(vecs, vec2, chosen_pairs, n=5, dropped=True, from_iter=0, to_iter=-1):
+    """
+    Get top pairs which change distance between words the most.
+    """
+    
+    vecs_diff = vecs[to_iter, :] - vecs[from_iter, :]
+    args_sorted = np.argsort(vecs_diff)
+
+    for i in xrange(n):
+        if (dropped):
+            idx = args_sorted[i]
+        else:
+            idx = args_sorted[-1-i]
+        print("Words:", chosen_pairs[idx])
+        print("Assesor score:", vec2[idx])
+        print("Distance change:", vecs[from_iter, idx], '-->', vecs[to_iter, idx])
+        print('\n')
+    
+    
+def nearest_words_from_iter(model, word, from_folder, top=20, display=False, it=1):
+    """
+    Get top nearest words from some iteration of optimization method.
+    """
+    C, W = model.load_CW(from_folder=from_folder, iteration=it)
+
+    model.W = W.copy()
+    model.C = C.copy()
+
+    nearest_sum = model.nearest_words(word, top, display)
+    
+    return nearest_sum
+
+################################## Analogical reasoning experiments ##################################
+
+def argmax_fun(W, indices, argmax_type='levi'):
+    """
+    cosine: b* = argmax cosine(b*, b - a + a*) 
+    levi: b* = argmax cos(b*,a*)cos(b*,b)/(cos(b*,a)+eps)
+    """
+    
+    if (argmax_type == 'levi'):
+        W = W / np.linalg.norm(W, axis=0)
+        words3 = W[:, indices]
+        cosines = ((words3.T).dot(W) + 1) / 2
+        obj = (cosines[1] * cosines[2]) / (cosines[0] + 1e-3)
+        pred_idx = np.argmax(obj)
+        
+    elif (argmax_type == 'cosine'):
+        words3_vec = W[:, indices].sum(axis=1) - 2*W[:, indices[0]]
+        W = W / np.linalg.norm(W, axis=0)
+        words3_vec = words3_vec / np.linalg.norm(words3_vec)
+        cosines = (words3_vec.T).dot(W)
+        pred_idx = np.argmax(cosines)
+        
+    return pred_idx
+
+def analogical_reasoning(model, dataset, from_folder, it=0):
+    """
+    Calculate analogical reasoning accuracy for given dataset.
+    """
+    dic = model.dictionary
+    
+    _, W = model.load_CW(from_folder, iteration=it)
+    W = W / np.linalg.norm(W, axis=0)
+
+    good_sum = 0
+    miss_sum = 0
+
+    for words in dataset.values:
+
+        a, b, a_, b_ = words
+
+        if (a in dic and b in dic and a_ in dic and b_ in dic):
+
+            indices = [dic[a], dic[b], dic[a_]]
+            
+            words3 = W[:, indices]
+            cosines = ((words3.T).dot(W) + 1) / 2
+            obj = (cosines[1] * cosines[2]) / (cosines[0] + 1e-3)
+            pred_idx = np.argmax(obj)
+            
+            if (model.inv_dict[pred_idx] == b_):
+                good_sum += 1
+        else: 
+            miss_sum += 1
+
+    # calculate accuracy
+    acc = (good_sum) / float(dataset.shape[0]-miss_sum)
+    
+    return acc, miss_sum
+
+def AR_experiment(model, dataset, from_folder, MAX_ITER=100, step_size=5, plot_accs=False):
+    """
+    Aggregator for analogical reasoning accuracy experiment.
+    """
+    
+    # Calculate accuracies 
+    accs = []
+    num_points = MAX_ITER/step_size + 1
+    for i in xrange(num_points):
+        acc, miss = analogical_reasoning(model, dataset, from_folder, i*step_size)
+        accs.append(acc)
+    accs = np.array(accs)
+    
+    # Plot accuracies
+    if (plot_accs):
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(np.arange(num_points)*step_size, accs)
+        ax.set_ylabel('Accuracy', fontsize=14)
+        ax.set_xlabel('Iterations', fontsize=14)
+        ax.grid()    
+        
+    return accs, miss
---
+++ tmp/visualize.py
@@ -0,0 +1,146 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import glob
+from scipy.sparse.linalg import svds
+from numpy.linalg import svd
+from scipy.spatial.distance import cosine
+from scipy.stats import spearmanr
+from sklearn.preprocessing import normalize
+from util.word2vec_as_MF import Word2vecMF
+
+
+
+################################## Spearman correlation ########################################
+def correlation(model, benchmark, from_folder, index, plot_corrs=False):
+    """
+    Aggregator for word similarity correlation experiment.
+    """
+    
+    # Load dataset and model dictionary
+
+    dataset = pd.read_csv(benchmark,header=None, delimiter=';').values
+    model_vocab = model.vocab
+    # Choose only pairs of words which exist in model dictionary
+    ind1 = []
+    ind2 = []
+    vec2 = []
+    chosen_pairs = []
+    for i in range(dataset.shape[0]):
+        try:
+            word1 = dataset[i, 0].lower()
+            word2 = dataset[i, 1].lower()
+        except:
+            print(dataset[i,0])
+        if (word1 in model_vocab and word2 in model_vocab):
+            ind1.append(int(model_vocab[word1]))
+            ind2.append(int(model_vocab[word2]))
+            vec2.append(np.float64(dataset[i, 2]))
+            chosen_pairs.append((word1, word2))
+            
+    vec1 = []
+    C, W = model.load_CW(from_folder, index)
+
+    
+    normInv = np.array([1./n if n else 0. for n in np.linalg.norm(W, axis=0)])
+    W = W*normInv
+    vec1 = (W[:,ind1]*W[:,ind2]).sum(axis=0)
+
+    corr = spearmanr(vec1, vec2)[0]
+    #corr=0.7
+    '''
+    print('debug')
+    print(vec1)
+    print(np.isfinite(C).all())
+    print(np.isfinite(W).all())
+    print(np.isfinite(vec1).all())
+    print(np.isfinite(vec2).all())
+    '''
+    return corr, vec1, vec2, chosen_pairs
+
+def corr_word2vec(skip ,benchmark, model_vocab):
+    """
+    Calculate for word2vec model.
+    model_vocab, vocabulary dictionary
+    """
+     
+    
+    # Load dataset and model dictionary
+
+    dataset = benchmark.values
+
+    # Choose only pairs of words which exist in model dictionary
+    ind1 = []
+    ind2 = []
+    vec2 = []
+    chosen_pairs = []
+    for i in range(dataset.shape[0]):
+        try:
+            word1 = dataset[i, 0].lower()
+            word2 = dataset[i, 1].lower()
+        except:
+            print(dataset[i,0])
+        if (word1 in model_vocab and word2 in model_vocab):
+            ind1.append(int(model_vocab[word1]))
+            ind2.append(int(model_vocab[word2]))
+            vec2.append(np.float64(dataset[i, 2]))
+            chosen_pairs.append((word1, word2))
+            
+    vec1 = []
+    for pair in chosen_pairs:
+        word1, word2 = pair
+        vec1.append(skip.similarity(word1, word2))
+
+    corr = spearmanr(vec1, vec2)[0]
+    
+    return corr, vec1, vec2, chosen_pairs
+
+def datasets_corr(from_folder, MAX_ITER=1000, plot_corrs=False, matrix='W', train_ratio=1.0):
+    """
+    Calculate correlations for all datasets in datasets_path
+    """
+    
+    sorted_names = ['wordsim_sim', 'wordsim_rel', 'wordsim353','men3000','simlex999', 'rw2034', 'MTURK-771', 'rg65', 'verb143', 'mturk287', 'mc30']
+    #sorted_names = ['mc30', 'rg65']
+    
+    prefix='benchmark/'
+    corrs_dict = {}
+    filelist = glob.glob(from_folder+'/W*.npz')
+    steps = sorted([int(file.split('/')[-1][1:-4]) for file in filelist])
+    steps = [step for step in steps if step<MAX_ITER]
+    
+    model =  Word2vecMF()
+    model.vocab = model.load_vocab(from_folder+'/vocab.txt')[1]
+    for name in sorted_names:
+        
+        corrs = []
+        for idx, step in enumerate(steps):
+            try:
+                corrs.append(correlation(model=model,
+                             benchmark=prefix+name+'.csv',
+                             from_folder=from_folder,
+                             index=step)[0])
+            except:
+                corrs.append(np.inf)
+                print('Step', idx, 'invalid.', end=' ')
+
+        steps = [steps[i] for i,x in enumerate(corrs) if not np.isinf(x)]
+        corrs = [x for x in corrs if not np.isinf(x)]
+        corrs_dict[name]=(steps, corrs)
+        
+    column = 2
+    row = 6
+    fig, axarr = plt.subplots(row, column, sharex=True, figsize=(10, 10))
+    fig.tight_layout()
+    #fig.set_figheight(10)
+    if (plot_corrs):
+        for idx, name in enumerate(sorted_names):
+            i=idx//column
+            j=idx%column
+            axarr[i,j].plot(corrs_dict[name][0], corrs_dict[name][1])
+            axarr[i,j].set_title(name)
+    fig=axarr[0,0].figure
+    fig.text(0.01,0.5, "Linguistic scores (Spearman Correlation Scores)", ha="center", va="center",  rotation=90)
+    fig.text(0.5,0.0, "Number of Iterations", ha="center", va="center")
+    return corrs_dict
